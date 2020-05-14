eabe2a58556e9a718bcf06f71aa7636f717503ab
Magmusacy <xpfk.098@gmail.com>
2020-05-14 15:26:45 +0200

    Planned what I have to do for caesar cipher to work

---
+++ Caesar-cipher.rb
@@ -0,0 +1,13 @@
+=begin
+Pass a string and a number to Cipher method
+Convert string to an array with every letter as a different element
+Check if current element belongs to upper case letters or lower case letters
+    str = 77.chr            # "M"
+    s2  = 233.chr("UTF-8")  # "é"
+    num = "M".ord           # 77
+make us out of these methods ^^^
+make a method that checks if number of letters shifted is bigger than there are letters left in alphabet
+don't forget to wrap from z to a
+if letter has a charcode of for example 80 and we subtract initial value from it (in that case initial value is 65) we'll get a value of 15 which represents how many shifts we have
+to do from starting position to the current letter
+=end
